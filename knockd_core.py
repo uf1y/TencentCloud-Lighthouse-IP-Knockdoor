57c46c2abc6140c5a0ad579143f9646b45af7b86
uf1y <117698857+uf1y@users.noreply.github.com>
2023-10-22 17:54:25 +0800

    移除vpc_client, models

--- knockd_core.py
+++ knockd_core.py
@@ -14,7 +14,6 @@ from tencentcloud.common import credential
 from tencentcloud.common.profile.client_profile import ClientProfile
 from tencentcloud.common.profile.http_profile import HttpProfile
 from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentCloudSDKException
-from tencentcloud.vpc.v20170312 import vpc_client, models
 from tencentcloud.lighthouse.v20200324 import lighthouse_client, models as models_light
 
 import time
